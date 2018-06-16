a72ccee0b4ece8a255582a7cc5ce19a3eef660b6
pps <ledevzl@gmail.com>
2018-06-16 12:56:28 +0000

    quick save

--- src/components/SCsDropZone.jsx
+++ src/components/SCsDropZone.jsx
@@ -5,26 +5,39 @@ import './style.css';
 
 import Dropzone from "react-dropzone";
 
+// wrapper around console.log
+function cls(itemToLog) {
+  console.log(itemToLog);
+}
+
 class SCsDropZone extends Component {
 
-  render() {
-    return (
-    	
+    readJSONFile(f) {
+        cls("file read");
+        cls(f);
+    }
 
-        <div className="col-md-4">
-            <div className="card mb-4 box-shadow">
-                <div className="card-body">
-                    <div className="dropzone">
-                        <Dropzone>
-                        <p>
-                        Drop your smart contract's .json file here
-                        </p>
-                        </Dropzone>
+    render() {
+        return (
+            <div className="col-md-4">
+                <div className="card mb-4 box-shadow">
+                    <div className="card-body">
+                        <div className="dropzone">
+                            <Dropzone /* TODO style later*/
+                                accept="application/json"
+                                onDrop={(accepted, rejected) => {
+                                    if(accepted.length > 0){this.readJSONFile(accepted[accepted.length - 1]);}
+                                    if(rejected.length > 0){/* TODO show alert/info */}
+                                }}>
+                            <p>
+                            Drop your smart contract's .json file here
+                            </p>
+                            </Dropzone>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
   }
 }
 
